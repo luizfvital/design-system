6d9b98082a59780f0f15775ef3956080d4e2eb32
Luiz Fernando Vital <luizfvital@gmail.com>
2023-11-03 19:49:00 +0000

    feat: add stitches

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@luizfvital-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@luizfvital-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.primary300 }}>Hello World</h1>
+}
