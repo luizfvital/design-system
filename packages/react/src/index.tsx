c03ebe71f9940464eed38eb6ec5c459c198c68e9
Luiz Fernando Vital <luizfvital@gmail.com>
2023-11-03 20:00:22 +0000

    feat: initialize and configure stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,11 @@
-import { colors } from '@luizfvital-ui/tokens'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$gray200',
+  borderRadius: '$md',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.primary300 }}>Hello World</h1>
+  return <Button>Hello World</Button>
 }
